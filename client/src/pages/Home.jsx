82f70650db7266edcbd78b712ba77d2a408d6d81
BugHunter34 <adam.tomala@seznam.cz>
2026-06-01 16:00:40 +0000

    pull

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,13 +7,13 @@ export default function Home() {
   const API_URL = 'http://localhost:6000';
 
   useEffect(() => {
-    fetch('${API_URL}/')
+    fetch(`${API_URL}/`)
       .then(res => res.ok ? setStatus('Server Online') : setStatus('Server Offline'))
       .catch(() => setStatus('Server Offline'));
   }, []);
 
   const handleSubmit = async () => {
-    const res = await fetch('${API_URL}/echo', {
+    const res = await fetch(`${API_URL}/echo`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ text: input })
