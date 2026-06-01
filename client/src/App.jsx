6cd1cd000c7075b9762da9257d0b1d7fb2e6ce3f
GovisLost <vitalesak@proton.me>
2026-06-01 17:34:49 +0200

    some design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,13 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import '@picocss/pico/css/pico.min.css'; 
+import './index.css';
+import Header from './components/Header.jsx'
 
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-      
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         
