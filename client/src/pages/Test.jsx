edae4351f1b406eb6886152b6322591cabd548b8
BugHunter34 <adam.tomala@seznam.cz>
2026-06-02 20:31:28 +0000

    zbytecny commit v2

--- client/src/pages/Test.jsx
+++ client/src/pages/Test.jsx
@@ -20,15 +20,8 @@ export default function Test() {
 
 
     return(
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Error</title>
-        </head>
-        <body>
+        <div>
             {status}
-        </body>
-        </html>
+        </div>
     )
 }
